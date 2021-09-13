bffa82bdece4c4aec58e7f747ef419940c4db3fe
OussamaLajnaf <oussamalajnaf32@gmail.com>
2021-09-13 17:43:14 +0200

    second

--- src/Component/Profile/Photoprofile.js
+++ src/Component/Profile/Photoprofile.js
@@ -1,11 +1,11 @@
 
-import Photoprofile from "./image/imageReact.jpg"
+// import Photoprofile from "public/oussama.jpg"
 
 const Photoprofile = () => {
 
     return (
         <div>
-            <img className="img1" src="/image/imageReact.jpg" />
+            <img alt="" className="img1" src="/oussama.jpg" />
         
         </div>
     )
